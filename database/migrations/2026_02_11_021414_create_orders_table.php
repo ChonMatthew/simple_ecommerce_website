f3b2222f141f22f260ccde4d26be6bcafcbff27a
Your Name <you@example.com>
2026-02-11 10:42:55 +0800

    Add models and migrations

---
+++ database/migrations/2026_02_11_021414_create_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class, 'user_id')->nullable;
+            $table->decimal('total_price',20,2)->default(0);
+            $table->string('status', 45);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
