816b00974baa163dd6a00d32d0233e63179502d8
Your Name <you@example.com>
2026-02-12 13:38:46 +0800

    Created seeders for Products table

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    public function definition(): array
+    {
+        return [
+            'name'        => fake()->words(3, true),
+            'image'       => null,
+            'description' => fake()->paragraphs(2, true),
+            'price'       => fake()->randomFloat(2, 5, 200),
+        ];
+    }
+}
