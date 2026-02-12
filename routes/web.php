442a2f237293872816520b4e6413d59cb4108002
Your Name <you@example.com>
2026-02-12 19:09:20 +0800

    Setup for Vue.js implementation

--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,28 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return Inertia::render('Home');
+});
+
+//Route::get('/', [HomeController::class, 'index'])->name('home');
 
 // Public Routes (no login required)
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+//Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+//Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
 
 // Cart Routes (no login required)
-Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
-Route::post('/cart', [CartController::class, 'store'])->name('cart.store');
-Route::patch('/cart/{cart_item}', [CartController::class, 'update'])->name('cart.update');
-Route::delete('/cart/{cart_item}', [CartController::class, 'destroy'])->name('cart.destroy');
+// Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+// Route::post('/cart', [CartController::class, 'store'])->name('cart.store');
+// Route::patch('/cart/{cart_item}', [CartController::class, 'update'])->name('cart.update');
+// Route::delete('/cart/{cart_item}', [CartController::class, 'destroy'])->name('cart.destroy');
 
 // Order Routes (no login required)
-Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
-Route::get('/orders/{order}', [OrderController::class, 'show'])->name('orders.show');
-Route::post('/checkout', [OrderController::class, 'store'])->name('checkout');
+// Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
+// Route::get('/orders/{order}', [OrderController::class, 'show'])->name('orders.show');
+// Route::post('/checkout', [OrderController::class, 'store'])->name('checkout');
 
 // TODO: Add login required routes
 // // Cart Routes (login required)
